098bdd871551dae0f0c207c10348a6c57995b7e3
synthesis_himanshu <synthesis_himanshu@hotmail.com>
2022-02-17 11:12:10 +0530

    init

---
+++ src/webparts/azureTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAzureTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AzureTestWebPartStrings' {
+  const strings: IAzureTestWebPartStrings;
+  export = strings;
+}
